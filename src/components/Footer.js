a00fe10694bfcf232fec259583448d15bb25f914
naman-290 <66725877+naman-290@users.noreply.github.com>
2021-12-24 17:03:27 +0530

    f

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const footer = () => (
+  <footer className='footer'>
+    <div className='footer__copyright'>
+      &copy; 2021 Made with ❤️ by{' '}
+      <a className='footer__copyright--link' href='https://github.com/AndresXI'>
+        {' '}
+        Heybro
+      </a>
+    </div>
+  </footer>
+)
+
+export default footer
